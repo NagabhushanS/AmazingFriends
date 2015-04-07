35d01247bd8a53896fd31c719f38b9ae9b232eb4
Nagabhushan <nagabhushan1995@gmail.com>
2015-04-07 13:59:17 +0530

    Update ParseConstants.java

--- src/com/blogspot/nagabhushan/ParseConstants.java
+++ src/com/blogspot/nagabhushan/ParseConstants.java
@@ -5,6 +5,7 @@ public final class ParseConstants {
 	public static final String CLASS_MESSAGES = "Messages";
 	
 	// Field names
+	// The names are mainly related to the Parse Object keys
 	public static final String KEY_USER_ID = "userId";
 	public static final String KEY_USERNAME = "username";
 	public static final String KEY_FRIENDS_RELATION = "friendsRelation";
